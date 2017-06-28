09893c6f522c86184d6b5b66f3958ce1f6b04c17
Tudor Avram <a96tudor@gmail.com>
2017-06-28 11:04:12 +0800

    finished basic signin and signup

--- static/js/login.js
+++ static/js/login.js
@@ -6,6 +6,7 @@ function validate() {
 
   if (username == "admin" && password=="pass") {
     alert("Login Successfully")
+    send_request(JSON.stringify({"username": username, "password": password}))
     return true
   } else {
     attempts_remaining--;
@@ -23,5 +24,20 @@ function validate() {
     }
 
   }
+
+  function send_request(data) {
+    var xhr = new XMLHttpRequest();
+    var url = "http://127.0.0.1:5000/validate";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          var json = JSON.parse(xhr.responseText);
+          console.log(json.email + ", " + json.password);
+      }
+    };
+    xhr.send(data);
+  }
+
 
 }
