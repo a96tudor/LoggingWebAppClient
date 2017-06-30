d99333c7a041328ff70fa04a767d316ce3c0a01e
Tudor Avram <a96tudor@gmail.com>
2017-06-30 16:00:03 +0800

    finished integrating the webapp beta-version

--- static/js/start+stop.js
+++ static/js/start+stop.js
@@ -1,4 +1,4 @@
-var current_email = "";
+var pauseActive = false;
 
 function start_button_press(){
   var course_name = document.getElementById("course-name").value
@@ -21,7 +21,7 @@ function start_button_press(){
         if (!displayed_message) {
           current_email = email;
           displayed_message = true;
-
+          window.location.replace("working.html?"+email);
         }
       } else {
         if (xhr.responseText != "" &&  !displayed_message) {
@@ -42,11 +42,45 @@ function start_button_press(){
 }
 
 function pause_button_press() {
-  console.log(totalSeconds);
   pauseActive = !pauseActive;
   if (pauseActive) {
     document.getElementById("pause-button").innerHTML = "Resume";
   } else {
     document.getElementById("pause-button").innerHTML = "Pause";
   }
+}
+
+function done_button_press() {
+
+  var email = location.search.substr(1);
+  console.log(email);
+
+  var displayed_message = false;
+  if (confirm("You are about to submit. Are you sure you want to do this?")) {
+
+    var data_to_send = {
+      "email": email,
+      "time": totalSeconds
+    };
+
+    var xhr = new XMLHttpRequest();
+    var url = "http://127.0.0.1:5000/stop-work";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.status == 200) {
+        if (!displayed_message) {
+          alert("success");
+          window.location.replace("start.html");
+        }
+      } else {
+        if (xhr.responseText != "" &&  !displayed_message) {
+          alert("Something went wrong :(");
+          displayed_message = true;
+        }
+      }
+    };
+    xhr.send(JSON.stringify(data_to_send));
+  }
+
 }
