32f8c0e5ee4bf153d11a35238150478f3bf41569
Tudor Avram <a96tudor@gmail.com>
2017-06-30 13:23:53 +0800

    finished initial server integation

--- static/js/start+stop.js
+++ static/js/start+stop.js
@@ -1,8 +1,38 @@
 function start_button_press(){
   var course_name = document.getElementById("course-name").value
+  var email = document.getElementById("email").value
+  var displayed_message = false;
 
-  if (course_name) {
-    alert("All good!");
+  if (course_name && email) {
+
+    var data_to_send = {
+      "email": email,
+      "course": course_name
+    };
+
+    var xhr = new XMLHttpRequest();
+    var url = "http://127.0.0.1:5000/start-work";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.status == 200) {
+        if (!displayed_message) {
+          alert("All good!");
+          displayed_message = true;
+        }
+      } else {
+        if (xhr.responseText != "" &&  !displayed_message) {
+          alert(xhr.responseText);
+          displayed_message = true;
+        }
+        if (xhr.status != 500) {
+          document.getElementById("course-name").value = ''
+          document.getElementById("email").value = ''
+        }
+      }
+    };
+    xhr.send(JSON.stringify(data_to_send));
+
   } else {
     alert("No course name entered!");
   }
