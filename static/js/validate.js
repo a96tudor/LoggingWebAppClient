4be554d8337c3fa4aa42ece19aee6e9817383d45
Tudor Avram <a96tudor@gmail.com>
2017-07-12 12:18:15 +0800

    done JS support for user validation

--- static/js/validate.js
+++ static/js/validate.js
@@ -2,5 +2,27 @@ var BASE_URL = "https://www.neural-guide.me";
 
 function submitData() {
   let pass = document.getElementById("pass").value;
-  
+  let pass_confirmation = document.getElementById("pass_confirm").value;
+
+  if (pass && pass_confirmation) {
+    if (pass == pass_confirmation) {
+      let dataToSend = {
+        "id": location.search.substr(1),
+        "pass": pass
+      };
+      let url = BASE_URL + "/validate-user";
+      let xhr = new XMLHttpRequest();
+      xhr.setRequestHeader("Content-type", "application/json");
+      xhr.open("POST", url, true);
+      xhr.onreadystatechange = function () {
+        console.log("STATUS=" + xhr.status);
+        console.log("RESPONSE_TEXT=" + xhr.responseText);
+      };
+      xhr.send(JSON.stringify(dataToSend));
+    } else {
+      alert("The two passwords don't coincide!")
+    }
+  } else {
+    alert("Please enter a password!");
+  }
 }
